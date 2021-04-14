1520c3bc19d60506192e68c5ca4637301e1a44d1
Warkingg <75408449+Warkingg@users.noreply.github.com>
2021-04-14 12:45:00 +0700

    Add .gitignore

---
+++ src/main/java/com/example/clothingstoreprojectteam/ClothingStoreProjectTeamApplication.java
@@ -0,0 +1,13 @@
+package com.example.clothingstoreprojectteam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClothingStoreProjectTeamApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ClothingStoreProjectTeamApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/clothingstoreprojectteam/ClothingStoreProjectTeamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.clothingstoreprojectteam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClothingStoreProjectTeamApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
