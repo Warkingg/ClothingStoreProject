d41eb18972f2008313a4f332bb9d03d4450825c1
Warkingg <75408449+Warkingg@users.noreply.github.com>
2021-04-16 00:01:48 +0700

    Fix Product entity

--- src/main/java/com/example/clothingstoreprojectteam/model/Product.java
+++ src/main/java/com/example/clothingstoreprojectteam/model/Product.java
@@ -21,8 +21,6 @@ public class Product {
 
     private String size;
 
-    private String category;
-
     private String description;
 
     private String imgUrl;
@@ -33,7 +31,7 @@ public class Product {
 
     @ManyToOne
     @JoinColumn(name="category_id")
-    private Product product;
+    private Category category;
 
 
 }
